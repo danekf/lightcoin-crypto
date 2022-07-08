629ff33f1fe260ac153bfd1ebda1137c20c988db
Danek <danek.fill@outlook.com>
2022-07-08 15:46:27 -0400

    This commit will add transaction funcitonality and dry up the code

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance(){
+    let balance = 0;
+    for (let transaction of this.transactions){
+      balance += transaction.value;
+    }
+    return balance
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
+  constructor(amount, account){
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit(){
+    if(this.isAllowed()){
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+}
+
+class Deposit extends Transaction {
+  get value(){
+    return this.amount;
+  }
+  isAllowed(){
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value(){
+    this.isAllowed = true;
+    return -this.amount;
+  }
+
+  isAllowed(){
+    if(this.account.balance > this.amount){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(500.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log(myAccount.balance);
+console.log(myAccount.transactions);
+console.log('Ending Balance:', myAccount.balance);
